bf8c699e7db6f3113a8959191f5fb01d7a58a68b
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2019-09-03 17:44:33 +0200

    add cities list

--- app/src/main/java/com/example/gl_crash_course/ForecastApiConst.kt
+++ app/src/main/java/com/example/gl_crash_course/ForecastApiConst.kt
@@ -11,4 +11,23 @@ object ForecastApiConst {
     const val UPDATE_TIME: Int = 2
     const val ADAPTER_LIST_SIZE: Int = 10
     const val ADAPTER_SIZE_DIVIDE: Int = 3
+
+    val PL_CITIES_IDS = mapOf(
+        "Szczecin" to 3083829,
+        "Gorzów Wlkp." to 3098722,
+        "Zielona Góra" to 7530991,
+        "Gdańsk" to 7531002,
+        "Bydgoszcz" to 3102014,
+        "Poznań" to 7530858,
+        "Wrocław" to 3081368,
+        "Olsztyn" to 763166,
+        "Warszawa" to 756135,
+        "Łódź" to 3093133,
+        "Katowice" to 3096472,
+        "Suwałki" to 7530941,
+        "Białystok" to 858789,
+        "Lublin" to 765876,
+        "Kraków" to 3094802,
+        "Rzeszów" to 7530819
+    )
 }
